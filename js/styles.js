3073b59b8bdafc7d79cfeb4b1c39dfb35613ce9e
Danmark Mutai <danmark.chemuren@gmail.com>
2019-05-06 16:52:02 +0300

    Updated README, change some things in the css file and js file

--- js/styles.js
+++ js/styles.js
@@ -12,46 +12,47 @@ $(document).ready(function() {
                 $(".block4chidden").toggle();
             });
             $("#work1").hover(function() {
-                $(this).css("background-color", "#FFF");
+                $(this).css("background-color", "grey");
             }, function() {
                 $(this).css("background-color", "white");
             });
             $("#work2").hover(function() {
-                $(this).css("background-color", "#FFB6C1");
+                $(this).css("background-color", "lightblue");
             }, function() {
                 $(this).css("background-color", "white");
             });
             $("#work3").hover(function() {
-                $(this).css("background-color", "#FFB6C1");
+                $(this).css("background-color", "sandybrown");
             }, function() {
                 $(this).css("background-color", "white");
             });
             $("#work4").hover(function() {
-                $(this).css("background-color", "#FFB6C1");
+                $(this).css("background-color", "teal");
             }, function() {
                 $(this).css("background-color", "white");
             });
             $("#work5").hover(function() {
-                $(this).css("background-color", "#FFB6C1");
+                $(this).css("background-color", "steelblue");
             }, function() {
                 $(this).css("background-color", "white");
             });
             $("#work6").hover(function() {
-                $(this).css("background-color", "#FFB6C1");
+                $(this).css("background-color", "sandybrown");
             }, function() {
                 $(this).css("background-color", "white");
             });
             $("#work7").hover(function() {
-                $(this).css("background-color", "#FFB6C1");
+                $(this).css("background-color", "brown");
             }, function() {
                 $(this).css("background-color", "white");
             });
             $("#work8").hover(function() {
-                $(this).css("background-color", "#FFB6C1");
+                $(this).css("background-color", "peru");
             }, function() {
                 $(this).css("background-color", "white");
             });
             $("#submit").click(function() {
                         alert("Your message has been received!");
-                        alert("THANK YOU")
-                    }
+                        alert("THANK YOU! :)");
+                    });
+                });
